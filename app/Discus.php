a521ab4f9896fb121c04cbadce34dbe77cd79554
Jerry Tsai <jack4689@hotmail.com>
2018-07-10 07:54:59 +0800

    init

---
+++ app/Discus.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Discus extends Model
+{
+    public $primaryKey = 'id';
+    public $table = 'discuses';
+    public $fillable = ['title', 'content'];
+}
